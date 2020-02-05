6631f41362e79997166686b2f9a7d6ae841ac303
anomerova <Nomerova.A.V@sberbank.ru>
2020-02-05 18:42:05 +0300

    create first train file

--- model/train_data.js
+++ model/train_data.js
@@ -1,8 +1,6 @@
 const tf = require('@tensorflow/tfjs')
 const trainTestSplit = require('train-test-split')
 
-const [train, test] = trainTestSplit(arr, 0.8, 1234)
-
 // util function to normalize a value between a given range.
 function normalize(value, min, max) {
     if (min === undefined || max === undefined) {
@@ -14,7 +12,8 @@ function normalize(value, min, max) {
 // data can be loaded from URLs or local file paths when running in Node.js.
 const data_path = '../data_csv/cardio_train.csv'
 const [train, test] = trainTestSplit(data_path, 0.7, 1234)
-
+console.log(train, test)
+/*
 // Constants from training data
 const VX0_MIN = -18.885;
 const VX0_MAX = 18.065;
@@ -67,3 +66,101 @@ const testValidationData =
     tf.data.csv(TEST_DATA_PATH, {columnConfigs: {pitch_code: {isLabel: true}}})
         .map(csvTransform)
         .batch(TEST_DATA_LENGTH);
+
+const model = tf.sequential();
+model.add(tf.layers.dense({units: 250, activation: 'relu', inputShape: [8]}));
+model.add(tf.layers.dense({units: 175, activation: 'relu'}));
+model.add(tf.layers.dense({units: 150, activation: 'relu'}));
+model.add(tf.layers.dense({units: NUM_PITCH_CLASSES, activation: 'softmax'}));
+
+model.compile({
+    optimizer: tf.train.adam(),
+    loss: 'sparseCategoricalCrossentropy',
+    metrics: ['accuracy']
+})
+
+// Returns pitch class evaluation percentages for training data
+// with an option to include test data
+async function evaluate(useTestData) {
+  let results = {};
+  await trainingValidationData.forEachAsync(pitchTypeBatch => {
+    const values = model.predict(pitchTypeBatch.xs).dataSync();
+    const classSize = TRAINING_DATA_LENGTH / NUM_PITCH_CLASSES;
+    for (let i = 0; i < NUM_PITCH_CLASSES; i++) {
+      results[pitchFromClassNum(i)] = {
+        training: calcPitchClassEval(i, classSize, values)
+      };
+    }
+  });
+
+  if (useTestData) {
+    await testValidationData.forEachAsync(pitchTypeBatch => {
+      const values = model.predict(pitchTypeBatch.xs).dataSync();
+      const classSize = TEST_DATA_LENGTH / NUM_PITCH_CLASSES;
+      for (let i = 0; i < NUM_PITCH_CLASSES; i++) {
+        results[pitchFromClassNum(i)].validation =
+            calcPitchClassEval(i, classSize, values);
+      }
+    });
+  }
+  return results;
+}
+
+async function predictSample(sample) {
+  let result = model.predict(tf.tensor(sample, [1,sample.length])).arraySync();
+  var maxValue = 0;
+  var predictedPitch = 7;
+  for (var i = 0; i < NUM_PITCH_CLASSES; i++) {
+    if (result[0][i] > maxValue) {
+      predictedPitch = i;
+      maxValue = result[0][i];
+    }
+  }
+  return pitchFromClassNum(predictedPitch);
+}
+
+// Determines accuracy evaluation for a given pitch class by index
+function calcPitchClassEval(pitchIndex, classSize, values) {
+  // Output has 7 different class values for each pitch, offset based on
+  // which pitch class (ordered by i)
+  let index = (pitchIndex * classSize * NUM_PITCH_CLASSES) + pitchIndex;
+  let total = 0;
+  for (let i = 0; i < classSize; i++) {
+    total += values[index];
+    index += NUM_PITCH_CLASSES;
+  }
+  return total / classSize;
+}
+
+// Returns the string value for Baseball pitch labels
+function pitchFromClassNum(classNum) {
+  switch (classNum) {
+    case 0:
+      return 'Fastball (2-seam)';
+    case 1:
+      return 'Fastball (4-seam)';
+    case 2:
+      return 'Fastball (sinker)';
+    case 3:
+      return 'Fastball (cutter)';
+    case 4:
+      return 'Slider';
+    case 5:
+      return 'Changeup';
+    case 6:
+      return 'Curveball';
+    default:
+      return 'Unknown';
+  }
+}
+
+module.exports = {
+  evaluate,
+  model,
+  pitchFromClassNum,
+  predictSample,
+  testValidationData,
+  trainingData,
+  TEST_DATA_LENGTH
+}
+*/
