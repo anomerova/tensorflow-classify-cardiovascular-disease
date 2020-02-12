d44ae6dd10103997368535168b7a10956410bd69
anomerova <Nomerova.A.V@sberbank.ru>
2020-02-12 16:47:18 +0300

    change webpack config

--- src/model/train_data.js
+++ src/model/train_data.js
@@ -1,28 +1,17 @@
 import * as tf from '@tensorflow/tfjs'
 // import trainTestSplit from 'train-test-split'
 import papa from 'papaparse'
+import csv from './cardio_train.csv'
 
 export function train_model(){
-    async function getData() {
-        const response = await fetch('cardio_train.csv')
-        const reader = response.body.getReader()
-        const result = await reader.read() // raw array
-        const decoder = new TextDecoder('utf-8')
-        const csv = decoder.decode(result.value) // the csv text
-        const results = papa.parse(csv,  {
+        /*const results = papa.parse(csv,  {
             header: true,
             dynamicTyping: true,
-            step: function(row) {
-                console.log("Row:", row.data);
-            },
             complete: function() {
                 console.log("All done!");
             }
-        })
-        console.log(results.data)
-    }
-
-    getData()
+        })*/
+        console.log(csv)
 
     // const [trainSplit, testSplit] = trainTestSplit(dataSet, 0.7, 1234)
     // console.log('parseMe', parseMe)
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,15 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.csv$/,
+                loader: 'csv-loader',
+                options: {
+                    dynamicTyping: true,
+                    header: true,
+                    skipEmptyLines: true
+                }
             }
         ]
     },
